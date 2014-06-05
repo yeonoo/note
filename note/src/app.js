5e86de770c867a840b6724613cc6d45ca40da86f
yeonoo <ywoo818@gmail.com>
2014-06-05 17:07:52 +0900

    4

---
+++ note/src/app.js
@@ -0,0 +1,17 @@
+function autoSave(event){
+    
+    //컨텐츠의 내용 가져오기
+    var value = $('#contents').val();
+    
+    window.localStorage.setItem('note_contents', value);
+}
+
+function autoLoad(event){
+    $('#contents').val(window.localStorage.getItem('note_contents'));
+    
+}
+
+
+console.log(1);
+$('#contents').on('keyup', autoSave);
+$(window).on("load", autoLoad);
